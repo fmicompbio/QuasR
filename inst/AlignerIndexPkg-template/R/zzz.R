88fd9d22c38327e13bf8cdc7e3af9a42fbf364b6
Anita Lerch <anita.lerch@fmi.ch>
2011-11-04 16:31:25 +0000

    change of object in index list
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/QuasR@60072 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/AlignerIndexPkg-template/R/zzz.R
+++ inst/AlignerIndexPkg-template/R/zzz.R
@@ -2,31 +2,38 @@
 ###
 ###
 
-loadIndex <- function(){
-    index <- list(
-        name="@PROVIDERVERSION@-@ALIGNER@",
-        path=file.path(system.file("index", package="@PKGNAME@", "@GENOMENAME@")),
-        aligner="@ALIGNER@",
-        alignerversion="@ALIGNERVERSION@",
-        organism="@ORGANISM@",
-        provider="@PROVIDER@",
-        providerversion="@PROVIDERVERSION@",
-        sourceurl="@SRCDATAFILES@"
-    )
-}
+#TODO define index list only once
+# loadIndex <- function(){
+#     
+#     index <- list(
+#         name="@GENOMENAME@@",
+#         shortname="@PROVIDERVERSION@",
+#         path=system.file("alignerindex", package="@PKGNAME@", "@GENOMENAME@"),
+#         path=file.path(indexDir, "@GENOMENAME@"),
+#         aligner="@ALIGNER@",
+#         alignerversion="@ALIGNERVERSION@",
+#         organism="@ORGANISM@",
+#         provider="@PROVIDER@",
+#         providerversion="@PROVIDERVERSION@",
+#         sourceurl="@SRCDATAFILES@",
+#         md5sum="@MD5SUM@"
+#     )
+# }
 
 .onLoad <- function(libname, pkgname)
 {
     indexDir <- system.file("alignerIndex", package=pkgname, lib.loc=libname)
     index <- list(
-        name="@PROVIDERVERSION@",
+        name="@GENOMENAME@",
+        shortname="@PROVIDERVERSION@",
         path=file.path(indexDir, "@GENOMENAME@"),
         aligner="@ALIGNER@",
         alignerversion="@ALIGNERVERSION@",
         organism="@ORGANISM@",
         provider="@PROVIDER@",
         providerversion="@PROVIDERVERSION@",
-        sourceurl="@SRCDATAFILES@"
+        sourceurl="@SRCDATAFILES@",
+        md5sum="@MD5SUM@"
     )
 #    objname <- "@ALIGNERINDEXNAME@"
     objname <- "@PROVIDERVERSION@@ALIGNER@Index"
