546fe6774b52836e669472df18b26fb3bf67fa24
Michael Stadler <Michael.Stadler@fmi.ch>
2011-11-28 12:14:22 +0000

    constant length wig files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/QuasR@60904 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/export-functions.R
+++ R/export-functions.R
@@ -1,14 +1,17 @@
 # functions that export alignments or alignment-derived data to external files
 
-bamfileToWig <- function(bamFname, wigFname=NULL, width=100L) {
+bamfileToWig <- function(bamFname, wigFname=NULL, width=100L, shift=0L, trackname=NULL) {
   if(!file.exists(bamFname))
     stop(bamFname," not found.")
 
   if(is.null(wigFname))
     wigFname <- paste(sub(".bam$","",bamFname),".wig")
 
+  if(is.null(trackname))
+    trackname <- sub(".bam$","",basename(bamFname))
+
   .progressReport(paste("converting",bamFname,"to wig"), phase=-1)
-  res <- .Call(.bamfile_to_wig, bamFname, wigFname, as.integer(width))
+  res <- .Call(.bamfile_to_wig, bamFname, wigFname, as.integer(width), as.integer(shift), trackname)
   .progressReport(paste("successfully created",wigFname), phase=1)
 
   return(res)
--- src/export_wig.c
+++ src/export_wig.c
@@ -17,16 +17,20 @@
   remark: could support compression by linking to the zlibbioc package
   see: http://www.bioconductor.org/packages/2.9/bioc/vignettes/zlibbioc/inst/doc/UsingZlibbioc.pdf
 */
-SEXP bamfile_to_wig(SEXP bam_in, SEXP wig_out, SEXP width) {
-    if (!IS_CHARACTER(bam_in) || 1 != LENGTH(bam_in))
-        Rf_error("'bam_in' must be character(1)");
-    if (!IS_CHARACTER(wig_out) || 1 != LENGTH(wig_out))
-        Rf_error("'wig_out' must be character(1)");
-    if (!IS_INTEGER(width) || 1 != LENGTH(width))
-        Rf_error("'width' must be integer(1)");
+SEXP bamfile_to_wig(SEXP bam_in, SEXP wig_out, SEXP width, SEXP shift, SEXP trackname) {
+    if (!isString(bam_in)    || 1 != length(bam_in))
+        error("'bam_in' must be character(1)");
+    if (!isString(wig_out)   || 1 != length(wig_out))
+        error("'wig_out' must be character(1)");
+    if (!isInteger(width)    || 1 != length(width))
+        error("'width' must be integer(1)");
+    if (!isInteger(shift)    || 1 != length(shift))
+        error("'shift' must be integer(1)");
+    if (!isString(trackname) || 1 != length(trackname))
+        error("'trackname' must be character(1)");
     
-    int w=asInteger(width), beg=0, end=0x7fffffff;
-    int32_t ih=0, currTid=0, currPos=0;
+    int w=asInteger(width), s=asInteger(shift), beg=0, end=0x7fffffff;
+    int32_t ih=0, currTid=0, currTlen=0, currPos=0, hitPos=0;
     double currCount=0.0;  
     bam1_t *hit = bam_init1();
 
@@ -44,8 +48,12 @@ SEXP bamfile_to_wig(SEXP bam_in, SEXP wig_out, SEXP width) {
     if (fout == NULL)
 	error("could not create outfile: %s\n", wig_out);
 
+    // output track header
+    fprintf(fout, "track type=wiggle_0 name='%s' description='%s' visibility=full color='27,158,119' altColor='27,158,119' priority=100 autoscale=off gridDefault=on maxHeightPixels=128:128:11 graphType=bar yLineMark=0.0 yLineOnOff=off windowingFunction=maximum smoothingWindow=off\n", trackname, trackname);
+
     // start a new target
     currTid = 0;     // is always first in a sorted bam file
+    currTlen = fin->header->target_len[currTid]; // length of the current target
     currPos = w-1;   // end of current window (zero-based)
     currCount = 0.0; // sum of alignment weight in current window
     fprintf(fout, "fixedStep chrom=%s start=1 step=%d span=%d\n", fin->header->target_name[currTid], w, w);
@@ -53,6 +61,11 @@ SEXP bamfile_to_wig(SEXP bam_in, SEXP wig_out, SEXP width) {
     while( samread(fin, hit) > 0) {
 	if (hit->core.flag & BAM_FUNMAP) // skip unmapped reads
 	    continue;
+
+	// get (shifted) position of hit
+	hitPos = hit->core.pos + s;
+	if (hitPos > currTlen)
+	    hitPos == currTlen;
 
 	// get alignment inverse weight (aux tag "IH")
 	ih = get_inverse_weight(hit);
@@ -65,21 +78,25 @@ SEXP bamfile_to_wig(SEXP bam_in, SEXP wig_out, SEXP width) {
 	if (currTid != hit->core.tid) {
 	    // output last window on former target
 	    fprintf(fout, "%.1f\n", currCount);
+	    // output empty windows until end of target
+	    while ((currPos += w) < currTlen)
+		fprintf(fout, "0\n");
 	    // start a new target
 	    currTid = hit->core.tid;
+	    currTlen = fin->header->target_len[currTid];
 	    currPos = w-1;
 	    fprintf(fout, "fixedStep chrom=%s start=1 step=%d span=%d\n", fin->header->target_name[currTid], w, w);
 	    // output empty windows
-	    while ((currPos += w) < hit->core.pos)
+	    while ((currPos += w) < hitPos)
 		fprintf(fout, "0\n");
 	    // start new window
 	    currCount = 0.0;
 
-	} else	if (currPos < hit->core.pos) {
+	} else	if (currPos < hitPos) {
 	    // output last window
 	    fprintf(fout, "%.1f\n", currCount);
 	    // output empty windows
-	    while ((currPos += w) < hit->core.pos)
+	    while ((currPos += w) < hitPos)
 		fprintf(fout, "0\n");
 	    // start new window
 	    currCount = 0.0;
@@ -90,6 +107,9 @@ SEXP bamfile_to_wig(SEXP bam_in, SEXP wig_out, SEXP width) {
     }
     // output very last window
     fprintf(fout, "%.1f\n", currCount);
+    // output empty windows until end of target
+    while ((currPos += w) < currTlen)
+	fprintf(fout, "0\n");
 
     fclose(fout);
     bam_index_destroy(idx);
