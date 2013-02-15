e8e5a7d60a2fba9c986547c7e99ad746fc64b7be
Michael Stadler <Michael.Stadler@fmi.ch>
2013-02-15 09:15:02 +0000

    add R_NO_REMAP and STRICT_R_HEADERS to avoid naming conflicts under windows
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/QuasR@73552 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/count_junctions.h
+++ src/count_junctions.h
@@ -1,3 +1,8 @@
+// prevent remapping of "Rf_length" to "length" in Rdefines.h, which clashes with fstream::length
+#define R_NO_REMAP
+// prevent remapping e.g. Ralloc, which causes conflicts under windows
+#define STRICT_R_HEADERS 1
+
 #include <Rdefines.h>
 #include "samtools/sam.h"
 
