509df8a0fcd05d2fd1ad8d256c10eebf8d74dcc9
Michael Stadler <Michael.Stadler@fmi.ch>
2015-02-09 13:16:50 +0000

    v 1.7.9: added QuasR citation; added BiocParallel to Depends; added getListOfBiocParallelParam() utility function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/QuasR@99234 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utilities.R
+++ R/utilities.R
@@ -244,3 +244,41 @@ md5subsum <-
             }
         }), use.names=FALSE)
     }
+
+# return a list(2) of BiocParallel::BiocParallelParam for nested parallelization
+#  [[1]] is used to parallelize across files
+#  [[2]] is used to parallelize across threads (accessing a single file) -> has to be a MulticoreParam or SerialParam object
+# for downwards compatibility, a parallel::cluster object can be passed that will be used to create the BiocParallel parameter objects
+getListOfBiocParallelParam <- function(clObj=NULL) {
+    if(is.null(clObj)) { # no 'clObj' argument
+        if("package:BiocParallel" %in% search()) { # BiocParallel is loaded
+            bppl <- registered() # use registered backend(s)
+        } else {                                   # BiocParallel not loaded
+            bppl <- list(BiocParallel::SerialParam(), BiocParallel::SerialParam())
+        }
+    } else {             # have 'clObj' argument
+        if(inherits(clObj, "SOCKcluster")) {
+            # get node names
+            tryCatch({
+                nodeNames <- unlist(parallel::clusterEvalQ(clObj, Sys.info()['nodename']))
+            }, error = function(ex) {
+                message("FAILED")
+                stop("The cluster object does not work properly on this system. Please consult the manual of the package 'parallel'\n",call.=FALSE)
+            })
+            coresPerNode <- table(nodeNames)
+            # subset cluster object (represent each node just a single time)
+            clObjSub <- clObj[!duplicated(nodeNames)]
+            bppl <- if(min(coresPerNode)==1)
+                list(as(clObj, "SnowParam"), SerialParam())
+            else
+                list(as(clObjSub, "SnowParam"), MulticoreParam(workers=min(coresPerNode)))
+        } else if(is.list(clObj) && all(sapply(clObj, inherits, "BiocParallelParam"))) {
+            bppl <- clObj
+        }
+    }
+    if(length(bppl)==1)
+        bppl[[2]] <- BiocParallel::SerialParam()
+    if(!inherits(bppl[[2]], c("MulticoreParam","SerialParam")))
+        stop('Error configuring the parallel backend. The second registered backend (registered()[[2]] or clObj[[2]]) has to be of class "MulticoreParam" or "SerialParam"')
+    return(bppl[1:2])
+}
