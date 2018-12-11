f5d81acddde11a76b831b0ab0f8a2cd323315f54
mbstadler <stadler.michael@gmail.com>
2018-12-11 16:47:31 +0100

    v1.23.5: include R_ext/Boolean.h to prevent Rdefines.h defining TRUE/FALSE as int constants

--- src/idxstats_bam.h
+++ src/idxstats_bam.h
@@ -1,5 +1,7 @@
 #include <ctype.h>
 #include <assert.h>
+// include Boolean.h early, will define TRUE/FALSE enum prefent Rdefines.h from defining them as int constants
+#include <R_ext/Boolean.h>
 #include <Rdefines.h>
 #include "samtools/bam.h"
 #include "samtools/khash.h"
--- src/split_sam_chr.h
+++ src/split_sam_chr.h
@@ -1,3 +1,5 @@
+// include Boolean.h early, will define TRUE/FALSE enum prefent Rdefines.h from defining them as int constants
+#include <R_ext/Boolean.h>
 #include <Rdefines.h>
 #include "utilities.h"
 #include "samtools/sam.h"
