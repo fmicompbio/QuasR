c3e8fde9f33bf33272b5db0bdc240e2ebf0f7d12
mbstadler <stadler.michael@gmail.com>
2018-12-18 16:41:46 +0100

    setting the environment variable R_TESTS to an empty string as a workaround for using parallel::makeCluster in tests; without it, test work with devtools::test(), but not with R CMD check or devtools::check()

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
+# setting R_TESTS to empty string because of
+# https://github.com/hadley/testthat/issues/144
+# revert this when that issue in R is fixed.
+Sys.setenv("R_TESTS" = "")
+
 library(testthat)
 library(QuasR)
 
-test_check("QuasR", wrap = FALSE)
+test_check("QuasR")
