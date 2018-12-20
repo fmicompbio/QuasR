da5c61b5daf92b0fd9dd86414138eedff14bddf6
mbstadler <stadler.michael@gmail.com>
2018-12-20 09:38:53 +0100

    improve tests for qProfile

--- tests/testthat/test_qProfile.R
+++ tests/testthat/test_qProfile.R
@@ -1,6 +1,22 @@
 
 context("qProfile")
 
+test_that("qProfile correctly digests its arguments", {
+  expect_error(qProfile("error", gtfGr))
+  expect_error(qProfile(pSingle, "error"))
+  expect_error(qProfile(pSingle, qGenome, mask = "error"))
+  expect_error(qProfile(pSingle, qGenome, mask = qGenome))
+  expect_error(qProfile(pChipSingle, gtfGr, upstream = "error"))
+  expect_error(qProfile(pChipSingle, gtfGr, downstream = "error"))
+  expect_error(qProfile(pChipSingle, gtfGr, shift = "halfInsert"))
+  expect_error(qProfile(pChipSingle, gtfGr, shift = "error"))
+  expect_error(qProfile(pSingle, gtfGr))
+  expect_warning(qProfile(pSingle, qTiles, useRead = "first"))
+  expect_error(qProfile(pSingle, qGenome, upstream = c(100, 100)))
+  expect_error(qProfile(pSingle, qGenome, downstream = c(100, 100)))
+  expect_warning(qProfile(pSingle, qGenome, upstream = 100001))
+})
+
 test_that("qProfile works as expected", {
   requireNamespace("GenomicRanges")
   qreg <- GenomicRanges::resize(gtfGr, fix = "start", width = 200)
@@ -8,9 +24,9 @@ test_that("qProfile works as expected", {
   names(qreg) <- NULL
   
   # no shift
-  pr  <- qProfile(pChipSingle, qreg, upstream = 0, downstream = 199)
+  pr  <- qProfile(pChipSingle, qreg, upstream = 0, downstream = 199, shift = c(0, 0), clObj = clObj)
   cnt <- qCount(pChipSingle, qreg)
-  expect_equal(sum(cnt[,2]), unname(rowSums(pr[[2]])))
+  expect_equal(sum(cnt[,2]), unname(sum(pr[[2]])))
   expect_equal(sum(cnt[,3]), unname(rowSums(pr[[3]])))
 
   # shift
@@ -19,17 +35,23 @@ test_that("qProfile works as expected", {
   expect_equal(sum(cnt[,2]), unname(rowSums(pr[[2]])))
   expect_equal(sum(cnt[,3]), unname(rowSums(pr[[3]])))
   
-  # smart shift
-  pr  <- qProfile(pRnaPaired, qreg, upstream = 0, downstream = 199, shift = "halfInsert")
-  cnt <- qCount(pRnaPaired, qreg, shift = "halfInsert")
+  # smart shift, useRead
+  pr  <- qProfile(pRnaPaired, qreg, upstream = 0, downstream = 199, shift = "halfInsert", useRead = "first")
+  cnt <- qCount(pRnaPaired, qreg, shift = "halfInsert", useRead = "first")
   expect_equal(sum(cnt[,2]), unname(rowSums(pr[[2]])))
   expect_equal(sum(cnt[,3]), unname(rowSums(pr[[3]])))
 
-  # test includeSecondary
+  # includeSecondary
   cnt1 <- qCount(pPhiX, auxGr, includeSecondary = TRUE)[1,2]
   cnt2 <- qCount(pPhiX, auxGr, includeSecondary = FALSE)[1,2]
   pr1  <- qProfile(pPhiX, auxGr, upstream = 0, downstream = 5386, includeSecondary = TRUE)
   pr2  <- qProfile(pPhiX, auxGr, upstream = 0, downstream = 5386, includeSecondary = FALSE)
   expect_equal(cnt1, sum(pr1[[2]]))
   expect_equal(cnt2, sum(pr2[[2]]))
+  
+  # allelic
+  pr1 <- qProfile(pChipSingleSnps, qreg, collapseBySample = TRUE)
+  pr2 <- qProfile(pChipSingleSnps, qreg, collapseBySample = FALSE)
+  expect_identical(unname(pr1), unname(pr2))
+  expect_length(pr1, 7L)
 })
