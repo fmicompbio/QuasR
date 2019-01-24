691acf45170b5450966ebfe074f8b47d35915dea
mbstadler <stadler.michael@gmail.com>
2019-01-24 10:57:57 +0100

    fix errror in test

--- tests/testthat/test_createAlignments-functions.R
+++ tests/testthat/test_createAlignments-functions.R
@@ -14,8 +14,8 @@ test_that("samToSortedBamParallel works as expected", {
     cat("bamf: ", bamf, "\n")
     cat("samf: ", samf, "\n")
     cat("  first 10 lines:\n", paste(readLines(samf, n = 10L), collapse = "\n"), "\n")
-    cat("bamout: ", bamout, "\n")
     bamout <- QuasR:::samToSortedBamParallel(file = samf, destination = boutnext, p = 2L, cacheDir = NULL)
+    cat("bamout: ", bamout, "\n")
     expect_identical(bamout, paste0(boutnext, ".bam"))
     expect_identical(unname(alignmentStats(bamout)),
                      unname(alignmentStats(bamf)))
