514fc27bcf6c632fd29f973694cf34cbc9c9ac56
mbstadler <stadler.michael@gmail.com>
2019-01-24 11:09:20 +0100

    do not use Rsamtools::asSam, but use existing sam file instead; remove diagnostic messages

--- tests/testthat/test_createAlignments-functions.R
+++ tests/testthat/test_createAlignments-functions.R
@@ -1,22 +1,19 @@
 
 context("createAlignments-functions")
 
-bamf     <- pChipSingle@alignments$FileName[1]
-samf     <- Rsamtools::asSam(file = bamf)
-boutnext <- tempfile(tmpdir = "extdata")
+samf  <- system.file("unitTests", "cases", "ex1.sam.gz", package = "Rsamtools")
+bout1 <- tempfile(tmpdir = "extdata")
+bamf  <- Rsamtools::asBam(file = samf, destination = bout1)
+bout2 <- tempfile(tmpdir = "extdata")
 
 test_that("samToSortedBamParallel correctly digests its arguments", {
-    expect_error(QuasR:::samToSortedBamParallel(file = "nonexistent.sam", destination = boutnext, p = 2L, cacheDir = NULL))
-    expect_error(QuasR:::samToSortedBamParallel(file = samf,              destination = boutnext, p = 2L, cacheDir = "error"))
+    expect_error(QuasR:::samToSortedBamParallel(file = "nonexistent.sam", destination = bout2, p = 2L, cacheDir = NULL))
+    expect_error(QuasR:::samToSortedBamParallel(file = samf,              destination = bout2, p = 2L, cacheDir = "error"))
 })
 
 test_that("samToSortedBamParallel works as expected", {
-    cat("bamf: ", bamf, "\n")
-    cat("samf: ", samf, "\n")
-    cat("  first 10 lines:\n", paste(readLines(samf, n = 10L), collapse = "\n"), "\n")
-    bamout <- QuasR:::samToSortedBamParallel(file = samf, destination = boutnext, p = 2L, cacheDir = NULL)
-    cat("bamout: ", bamout, "\n")
-    expect_identical(bamout, paste0(boutnext, ".bam"))
+    bamout <- QuasR:::samToSortedBamParallel(file = samf, destination = bout2, p = 2L, cacheDir = NULL)
+    expect_identical(bamout, paste0(bout2, ".bam"))
     expect_identical(unname(alignmentStats(bamout)),
                      unname(alignmentStats(bamf)))
 })
