af1ab05e3b710ec9b30169525133cd24bea5a20e
mbstadler <stadler.michael@gmail.com>
2020-05-07 11:10:18 +0200

    update tests

--- tests/testthat/test_qProfile.R
+++ tests/testthat/test_qProfile.R
@@ -18,6 +18,7 @@ test_that("qProfile correctly digests its arguments", {
   expect_error(qProfile(pSingle, qGenome, upstream = c(100, 100)))
   expect_error(qProfile(pSingle, qGenome, downstream = c(100, 100)))
   expect_error(qProfile(pSingle, qGenome, binSize = "error"))
+  expect_error(qProfile(pSingle, qGenome, binSize = 10))
   expect_warning(qProfile(pSingle, qGenome, upstream = 100001))
 })
 
@@ -60,21 +61,13 @@ test_that("qProfile works as expected", {
   expect_length(pr1, 7L)
   
   # binSize
-  pr1 <- qProfile(pChipSingle, qreg, upstream = 100, downstream = 100)
-  pr2 <- qProfile(pChipSingle, qreg, upstream = 100, downstream = 100, binSize = 10)
-  expect_identical(colnames(pr2[[1]]), as.character(seq(-100, 100, by = 10)))
-  expect_identical(sapply(pr1[-1], sum), sapply(pr2[-1], sum))
-
-  # ... compare to qCount
-  cnt1 <- qCount(pChipSingle, resize(qreg, width = 201, fix = "start"))
-  expect_identical(cnt1[, -1], do.call(cbind, lapply(pr1[-1], rowSums)))
-
-  cnt2 <- qCount(pChipSingle, GRanges(seqnames(qreg),
-                               IRanges(ifelse(as.logical(strand(qreg) != "-"),
-                                              start(qreg) - 105, end(qreg) - 104),
-                                       ifelse(as.logical(strand(qreg) != "-"),
-                                              start(qreg) + 104, end(qreg) + 105),
-                                       names = names(qreg)),
-                               strand = strand(qreg)))
-  expect_identical(cnt2[, -1], do.call(cbind, lapply(pr2[-1], rowSums)))
+  qreg2 <- reduce(qreg + 200) - 200
+  names(qreg2) <- seq_along(qreg2)
+  pr1 <- qProfile(pChipSingle, qreg2, upstream = 104, downstream = 104)
+  pr2 <- qProfile(pChipSingle, qreg2, upstream = 100, downstream = 100, binSize = 11)
+  expect_identical(colnames(pr2[[1]]), as.character(seq(-99, 99, by = 11)))
+  expect_identical(sapply(pr1[-1], rowSums), sapply(pr2[-1], rowSums))
+  cnt <- qCount(pChipSingle, promoters(qreg2, upstream = 104, downstream = 105))
+  expect_identical(cnt[, -1], do.call(cbind, lapply(pr1[-1], rowSums)))
+  expect_identical(cnt[, -1], do.call(cbind, lapply(pr2[-1], rowSums)))
 })
