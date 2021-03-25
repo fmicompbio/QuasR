257d76958b1b0f6dc0e1233ae8a785e0233151d1
Charlotte Soneson <charlottesoneson@gmail.com>
2021-03-25 15:57:33 +0100

    R script + roxygenize QuasR-package

---
+++ R/QuasR-package.R
@@ -0,0 +1,27 @@
+#' Quantify and Annotate Short Reads in R
+#'
+#' This package provides a pipeline for the quantification and analysis of 
+#' short reads.
+#'
+#' See packageDescription('QuasR') for package details.
+#' 
+#' @author Anita Lerch, Charlotte Soneson, Dimos Gaidatzis and Michael Stadler
+#' 
+#' @aliases QuasR QuasR-package
+#' @name QuasR-package
+#' 
+#' @keywords package
+#' 
+#' @seealso \code{\link{qAlign}}, \code{\link{qCount}}, \code{\link{qProfile}}, 
+#'   \code{\link{qMeth}}, \code{\link{qQCReport}}
+#' 
+#' @docType package
+#' 
+#' @examples 
+#' \dontrun{
+#' # see qCount, qMeth and qProfile manual pages for examples
+#' example(qCount)
+#' example(qMeth)
+#' example(qProfile)
+#' }
+NULL
