cce5a0476e3418c71957c9cae20cb337a39f87a3
mbstadler <stadler.michael@gmail.com>
2025-02-21 10:54:42 +0100

    fix typo

--- src/count_alignments.h
+++ src/count_alignments.h
@@ -1,14 +1,14 @@
-// include Boolean.h early, will define TRUE/FALSE enum prefent Rdefines.h from defining them as int constants
+// include Boolean.h early, will define TRUE/FALSE enum prevent Rdefines.h from defining them as int constants
 #include <R_ext/Boolean.h>
 #include <Rdefines.h>
 #include "htslib/sam.h"
 #include "utilities.h"
 
-SEXP count_alignments_non_allelic(SEXP bamfile, SEXP tid, SEXP start, SEXP end, SEXP strand, 
+SEXP count_alignments_non_allelic(SEXP bamfile, SEXP tid, SEXP start, SEXP end, SEXP strand,
                       SEXP selectReadPosition, SEXP readBitMask, SEXP shift, SEXP broaden, SEXP includeSpliced,
                       SEXP mapqMin, SEXP mapqMax, SEXP absIsizeMin, SEXP absIsizeMax);
 
-SEXP count_alignments_allelic(SEXP bamfile, SEXP tid, SEXP start, SEXP end, SEXP strand, 
+SEXP count_alignments_allelic(SEXP bamfile, SEXP tid, SEXP start, SEXP end, SEXP strand,
                       SEXP selectReadPosition, SEXP readBitMask, SEXP shift, SEXP broaden, SEXP includeSpliced,
                       SEXP mapqMin, SEXP mapqMax, SEXP absIsizeMin, SEXP absIsizeMax);
 
